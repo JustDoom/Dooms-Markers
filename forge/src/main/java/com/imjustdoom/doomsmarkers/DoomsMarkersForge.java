4d769a6601b277cfac2cee8bd15e6f2d9176b458
JustDoom <61824552+JustDoom@users.noreply.github.com>
2026-03-21 14:07:29 +1000

    Actually update Forge with new command argument

--- forge/src/main/java/com/imjustdoom/doomsmarkers/DoomsMarkersForge.java
+++ forge/src/main/java/com/imjustdoom/doomsmarkers/DoomsMarkersForge.java
@@ -2,6 +2,8 @@ package com.imjustdoom.doomsmarkers;
 
 import com.imjustdoom.doomsmarkers.command.argument.MarkerArgument;
 import com.imjustdoom.doomsmarkers.command.argument.MarkerArgumentInfo;
+import com.imjustdoom.doomsmarkers.command.argument.MarkerLiteArgument;
+import com.imjustdoom.doomsmarkers.command.argument.MarkerLiteArgumentInfo;
 import net.minecraft.commands.synchronization.ArgumentTypeInfo;
 import net.minecraft.commands.synchronization.ArgumentTypeInfos;
 import net.minecraft.core.registries.Registries;
@@ -17,6 +19,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class DoomsMarkersForge {
     private static final DeferredRegister<ArgumentTypeInfo<?, ?>> COMMAND_ARGUMENT_TYPES = DeferredRegister.create(Registries.COMMAND_ARGUMENT_TYPE, DoomsMarkers.MOD_ID);
     public static final RegistryObject<ArgumentTypeInfo<?, ?>> MARKER_ARGUMENT = COMMAND_ARGUMENT_TYPES.register("marker", () -> ArgumentTypeInfos.registerByClass(MarkerArgument.class, new MarkerArgumentInfo()));
+    public static final RegistryObject<ArgumentTypeInfo<?, ?>> MARKER_LITE_ARGUMENT = COMMAND_ARGUMENT_TYPES.register("marker_lite", () -> ArgumentTypeInfos.registerByClass(MarkerLiteArgument.class, new MarkerLiteArgumentInfo()));
 
     public DoomsMarkersForge() {
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
